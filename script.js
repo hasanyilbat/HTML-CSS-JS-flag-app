bb71d30ccb6d12b9ae7c8cb797c0cdec1d5c5d6d
Hasan <hasan.yilbat@gmail.com>
2022-06-17 15:59:51 +0300

    flag-app input(%50)

---
+++ script.js
@@ -0,0 +1,77 @@
+const fetchCountry = async (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      renderError(`Something went wrong ${res.status}`);
+      throw new Error();
+    }
+    const data = await res.json();
+    renderCountry(data[0]);
+    allCountryNames(data[0]);
+    console.log(typeof data);
+  } catch (error) {
+    // console.log(error);
+  }
+};
+
+const renderError = (err) => {
+  const countriesDiv = document.querySelector(".countries");
+  countriesDiv.innerHTML = `
+     <h1 class="text-danger">${err}</h1>
+     <img src="./img/404.png" alt="" />
+    `;
+};
+
+const renderCountry = (country) => {
+  const countriesDiv = document.querySelector(".countries");
+
+  const {
+    name: { common },
+    region,
+    capital,
+    languages,
+    flags: { svg },
+    currencies,
+  } = country;
+
+  countriesDiv.innerHTML += `<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${svg}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${common}</h5>
+    <p class="card-text">${region}</div>
+  <ul class="list-group list-group-flush">
+      <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${capital}</li>
+     <li class="list-group-item"> <i class="fas fa-lg fa-comments"></i> ${Object.values(
+       languages
+     )}</li>
+       <li class="list-group-item"> <i class="fas fa-lg fa-money-bill-wave"></i> ${
+         Object.values(currencies)[0].name
+       }, ${Object.values(currencies)[0].symbol} </li>
+  </ul>
+</div>`;
+};
+
+const countriesDiv = document.querySelector(".countries");
+const input = document.querySelector(".input");
+const countryArray = [];
+const getCountryName = async () => {
+  const res = await fetch(`https://restcountries.com/v3.1/all
+`);
+  const dataForInput = await res.json();
+  dataForInput.forEach((obj) => {
+    countryArray.push(
+      `<option value="${obj.name.common}">${obj.name.common}</option>`
+    );
+
+    console.log(countriesDiv.value);
+  });
+
+  const stringNames = countryArray.join("");
+  input.innerHTML = `<select class="form-select" aria-label="Default select example">
+ ${stringNames}
+</select>`;
+};
+
+getCountryName();
